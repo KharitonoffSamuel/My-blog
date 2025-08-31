97e5f5cb04f514bd40b0342342879a5c87d79cd1
KharitonoffSamuel <samuel.kharitonoff@edu.esiee.fr>
2025-08-31 23:26:13 +0200

    Add automatic translation

--- push-local.sh
+++ push-local.sh
@@ -9,6 +9,9 @@ fi
 # Stocke le message de commit
 COMMIT_MESSAGE="$1"
 
+# Supprime tous les fichiers se terminant par .fr.md dans les sous-dossiers
+find . -type f -name "*.fr.md" -delete
+
 # Ajoute tous les fichiers modifiés
 git add .
 
